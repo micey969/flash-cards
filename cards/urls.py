5c6342fd3efe26de7c0b248625c6f1f4e629534c
micey969 <micey_969@hotmail.com>
2022-08-08 19:51:12 -0400

    Added CRUD views

--- cards/urls.py
+++ cards/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.CardListView.as_view(), name='card-list'),
+    path('', views.CardListView.as_view(), name="card-list"),
+    path('new', views.CardCreateView.as_view(), name="card-create"),
+    path('edit/<int:pk>',views.CardUpdateView.as_view(), name="card-update"),
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ( ListView, )
+from django.views.generic import ( ListView, CreateView, UpdateView, )
+from django.urls import reverse_lazy
 from .models import Card
 
 # Create your views here.
@@ -6,3 +7,10 @@ class CardListView(ListView):
     model =  Card
     queryset = Card.objects.all().order_by("box", "-date_created") #the dash in front date created means descending order
     
+class CardCreateView(CreateView):
+    model = Card
+    fields = ["question", "answer", "box"]
+    success_url = reverse_lazy("card-create")
+
+class CardUpdateView(CardCreateView, UpdateView):
+    success_url = reverse_lazy("card-list")
